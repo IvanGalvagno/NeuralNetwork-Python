f9a9479457001cedda23d8d3d79a877528981efc
Ivan Galvagno <ivan.galvagno@gmail.com>
2017-11-10 17:22:09 -0200

    Init Trainning Code

--- main.py
+++ main.py
@@ -25,3 +25,18 @@ np.random.seed(1)
 #Inicialização dos Pesos randomicamente
 Syn0 = 2*np.random.random((3,1)) -1
 
+for iter in range(1000):
+    #Forward Propagation
+    l0 = X   #Layer 0
+    l1 = nonlin(np.dot(l0,Syn0)) #Hidden Layer
+
+    #Erro
+    l1_error = y - l1
+
+    l1_delta = l1_error * nonlin(l1,True)
+
+    #Atualizacao dos Pesos W
+    Syn0 += np.dot(l0.T, l1_delta)
+
+print('Saida após o treinamento: ')
+print(l1)
