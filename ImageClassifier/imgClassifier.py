ea71ac90eeba3a28fda9fdbe3beb784873ae38c3
Ivan Galvagno <ivan.galvagno@gmail.com>
2017-11-10 18:28:47 -0200

    Comment Explain the Output

--- ImageClassifier/imgClassifier.py
+++ ImageClassifier/imgClassifier.py
@@ -6,3 +6,13 @@ i = Image.open('ImageClassifier/images/dot.png')
 #Transforma a Imagem e Array, Aonde cada bloco é uma Linha da imagem
 iar = np.asarray(i)
 print(iar)
+# UM PIXEL DA IMAGEM .DOT que é 8x8
+#    R    G   B  Alpha
+#   [255 255 255 255]
+#   [255 255 255 255]
+#   [255 255 255 255]
+#   [255 255 255 255]
+#   [255 255 255 255]
+#   [255 255 255 255]
+#   [255 255 255 255]
+#   [255 255 255 255]
