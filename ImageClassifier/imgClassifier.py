0cf0b135ac94b10f663c10e81ef78eb0b861b0f7
Ivan Galvagno <ivan.galvagno@gmail.com>
2017-11-14 17:11:51 -0200

    Import Functools

--- ImageClassifier/imgClassifier.py
+++ ImageClassifier/imgClassifier.py
@@ -3,6 +3,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import time
+import functools
 
 #import image
 # i = Image.open('ImageClassifier/images/numbers/y0.4.png')
@@ -28,20 +29,20 @@ import time
 # plt.show(imgplot)
 
 def threshold(imageArray):
-    blanceAr = []
+    balanceAr = []
     newAr = imageArray
 
     for eachRow in imageArray:
         for eachPix in eachRow:
             # print(eachPix)
             # time.sleep(1)
-            avgNum = reduce(lambda x,y: x+y, eachPix[:3]) / len(eachPix[:3])
-            blanceAr.append(avgNum)
-    balance = reduce(lambda x,y: x+y, blanceAr) / len(blanceAr)
+            avgNum = functools.reduce(lambda x,y: x+y, eachPix[:3]) / len(eachPix[:3])
+            balanceAr.append(avgNum)
+    balance = functools.reduce(lambda x,y: x+y, balanceAr) / len(balanceAr)
 
     for eachRow in newAr:
         for eachPix in eachRow:
-            if( reduce(lambda x,y: x+y, eachPix[:3]) / len(eachPix[:3]) > balance):
+            if( functools.reduce(lambda x,y: x+y, eachPix[:3]) / len(eachPix[:3]) > balance):
                 eachPix[0] = 255  #RED
                 eachPix[1] = 255  #GREEN
                 eachPix[2] = 255  #BLUE
@@ -69,17 +70,16 @@ iar4 = np.array(i4)
 
 threshold(iar3)
 
-# fig = plt.figure()
-# ax1 = plt.subplot2grid((8,6), (0,0), rowspan=4, colspan=3)
-# ax2 = plt.subplot2grid((8,6), (4,0), rowspan=4, colspan=3)
-# ax3 = plt.subplot2grid((8,6), (0,3), rowspan=4, colspan=3)
-# ax4 = plt.subplot2grid((8,6), (4,3), rowspan=4, colspan=3)
+fig = plt.figure()
+ax1 = plt.subplot2grid((8,6), (0,0), rowspan=4, colspan=3)
+ax2 = plt.subplot2grid((8,6), (4,0), rowspan=4, colspan=3)
+ax3 = plt.subplot2grid((8,6), (0,3), rowspan=4, colspan=3)
+ax4 = plt.subplot2grid((8,6), (4,3), rowspan=4, colspan=3)
+ax1.imshow(iar)
+ax2.imshow(iar2)
+ax3.imshow(iar3)
+ax4.imshow(iar4)
 
-# ax1.imshow(iar)
-# ax2.imshow(iar2)
-# ax3.imshow(iar3)
-# ax4.imshow(iar4)
-
-# plt.show()
+plt.show()
 
 
